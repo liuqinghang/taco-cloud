a61c2ce2a8f31752350c7bd41c64035c422c020f
liuqinghang <2326713669@qq.com>
2021-03-10 22:53:13 +0800

    增加Homecontroller

--- taco-cloud/src/main/java/com/lutong/controller/HomeController.java
+++ taco-cloud/src/main/java/com/lutong/controller/HomeController.java
@@ -1,8 +1,12 @@
-package com.lutong.tacocloud;
+package com.lutong.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * @author 路瞳
+ * @Data
+ */
 @Controller
 public class HomeController {
     @GetMapping("/")
--- taco-cloud/src/test/java/com/lutong/tacocloud/HomeControllerTest.java
+++ taco-cloud/src/test/java/com/lutong/tacocloud/HomeControllerTest.java
@@ -1,5 +1,6 @@
 package com.lutong.tacocloud;
 
+import com.lutong.controller.HomeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
