5028e43dee6d8ce2d1daa1e252d36b33ec9f4a90
liuqinghang <2326713669@qq.com>
2021-03-11 10:09:17 +0800

    taco初始化

--- taco-cloud/src/main/java/com/lutong/tacocloud/HomeController.java
+++ taco-cloud/src/main/java/com/lutong/tacocloud/HomeController.java
@@ -1,5 +1,6 @@
-package com.lutong.controller;
+package com.lutong.tacocloud;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,11 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author 路瞳
  * @Data
  */
+@ComponentScan
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String home() {
-
+//返回视图名
         return "home";
     }
 }
